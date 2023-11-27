4c1765ae40039067cdbed1c6e2d7e260a02b426d
kkw2758 <dtc3229@gmail.com>
2023-11-27 17:30:36 +0900

    Feat(Lotto) : 로또 도메인에 대한 예외처리 코드 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,43 @@
 package lotto.domain;
 
 import java.util.List;
+import lotto.exception.ErrorMessage;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private final static int LOTTO_SIZE = 6;
+    private final List<LottoNumber> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = numbers.stream()
+                .map(LottoNumber::from)
+                .toList();
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        validateLength(numbers);
+        validateDuplicate(numbers);
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        if (checkDuplicate(numbers)) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_MEMBER_IN_LOTTO.getMessage());
         }
     }
 
-    // TODO: 추가 기능 구현
+    private boolean checkDuplicate(List<Integer> numbers) {
+        return numbers.stream()
+                .distinct()
+                .count() != numbers.size();
+    }
+
+    private void validateLength(List<Integer> numbers) {
+        if (checkLottoLength(numbers)) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_LENGTH.getMessage());
+        }
+    }
+
+    private boolean checkLottoLength(List<Integer> numbers) {
+        return numbers.size() != LOTTO_SIZE;
+    }
 }
--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -2,7 +2,10 @@ package lotto.exception;
 
 
 public enum ErrorMessage {
-    INVALID_LOTTO_NUMBER("로도 번호의 숫자의 범위는 1 ~ 45 까지입니다.");
+    INVALID_LOTTO_NUMBER("로도 번호의 숫자의 범위는 1 ~ 45 까지입니다."),
+    INVALID_LOTTO_LENGTH("로또 번호의 길이는 6이여야 합니다."),
+    DUPLICATE_MEMBER_IN_LOTTO("로또에 중복된 번호가 포함되어 있습니다.");
+
 
     private final String message;
     private final String PREFIX = "[ERROR] ";
