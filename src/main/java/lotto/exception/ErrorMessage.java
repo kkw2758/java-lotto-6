856a171bbfe6b2c0e7d9cb15355a90ad3f209cb3
kkw2758 <dtc3229@gmail.com>
2023-11-27 18:06:17 +0900

    Feat(BonusNumber) : 보너스 번호를 관리하는 BonusNubmer 도메인 추가

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -5,7 +5,8 @@ public enum ErrorMessage {
     INVALID_LOTTO_NUMBER("로도 번호의 숫자의 범위는 1 ~ 45 까지입니다."),
     INVALID_LOTTO_LENGTH("로또 번호의 길이는 6이여야 합니다."),
     DUPLICATE_MEMBER_IN_LOTTO("로또에 중복된 번호가 포함되어 있습니다."),
-    INVALID_MONEY("로도 구입 금액은 1,000원으로 나누어 떨어져야 합니다.");
+    INVALID_MONEY("로도 구입 금액은 1,000원으로 나누어 떨어져야 합니다."),
+    INVALID_BONUS_NUMBER("보너스 번호는 당첨 번호에 포함될 수 없습니다.");
 
 
     private final String message;
