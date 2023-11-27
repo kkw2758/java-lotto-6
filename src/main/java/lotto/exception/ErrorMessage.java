64ef0065bf6e0fa8b7affc8962e28b3b3770501a
kkw2758 <dtc3229@gmail.com>
2023-11-27 17:04:56 +0900

    Feat : MVC패턴 기본 패키지 및 클래스 생성

---
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -0,0 +1,17 @@
+package lotto.exception;
+
+
+public enum ErrorMessage {
+    ERROR_MESSAGE("MESSAGE_INFO");
+
+    private final String message;
+    private final String PREFIX = "[ERROR] ";
+
+    ErrorMessage(String message) {
+        this.message = PREFIX + message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
