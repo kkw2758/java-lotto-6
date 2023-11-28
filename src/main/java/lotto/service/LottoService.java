06d61e7c7c36135f665d73bb409fc205168ed812
kkw2758 <dtc3229@gmail.com>
2023-11-28 19:59:53 +0900

    Feat(LottoService) : 각 등수 별 당첨 내역을 구한다.

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,9 +1,13 @@
 package lotto.service;
 
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
+import lotto.domain.BonusNumber;
 import lotto.domain.Lotto;
 import lotto.domain.Money;
+import lotto.domain.Rank;
 import lotto.utils.RandomUtil;
 
 public class LottoService {
@@ -16,5 +20,14 @@ public class LottoService {
             lottos.add(new Lotto(RandomUtil.generateRandomNumbers(1, 45, 6)));
         }
         return lottos;
+    }
+
+    public Map<Rank, Integer> makeRankResult(List<Lotto> lottos, Lotto answerLotto, BonusNumber bonusNumber) {
+        Map<Rank, Integer> rankResult = new EnumMap<Rank, Integer>(Rank.class);
+        for (Lotto lotto : lottos) {
+            Rank rank = Rank.findRank(lotto.calculateMatchCount(answerLotto), lotto.has(bonusNumber.getValue()));
+            rankResult.put(rank, rankResult.getOrDefault(rank, DEFAULT_VALUE) + 1);
+        }
+        return rankResult;
     }
 }
