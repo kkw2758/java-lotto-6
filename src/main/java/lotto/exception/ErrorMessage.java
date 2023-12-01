e17efeed8bf9325f9b7f48a4c8a24b93d25204f0
kkw2758 <dtc3229@gmail.com>
2023-12-01 09:40:55 +0900

    Feat(LottoNumber) : 로또 번호의 숫자 범위가 1 ~ 45 사이를 벗어나면 예외 처리 한다.

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -2,7 +2,8 @@ package lotto.exception;
 
 public enum ErrorMessage {
     NOT_NUMBER("숫자를 입력해 주세요."),
-    INVALID_MONEY("구입 금액은 1,000원으로 나누어 져야합니다.");
+    INVALID_MONEY("구입 금액은 1,000원으로 나누어 져야합니다."),
+    INVALID_LOTTO_NUMBER("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
 
     private final String message;
     private final String PREFIX = "[ERROR] ";
