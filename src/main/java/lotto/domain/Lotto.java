5729f838c5baa45d889a4a0cdfd938daabc19160
kkw2758 <dtc3229@gmail.com>
2023-12-01 09:50:54 +0900

    Feat(Lotto) : 로또의 번호가 중복되거나 로또의 길이가 6이 아니라면 예외 처리한다

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,41 @@
+package lotto.domain;
+
+import java.util.List;
+import lotto.exception.ErrorMessage;
+
+public class Lotto {
+    private static final int LOTTO_SIZE = 6;
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        validateLottoSize(numbers);
+        validateLottoMember(numbers);
+    }
+
+    private void validateLottoMember(List<Integer> numbers) {
+        if (checkLottoHasDuplicateMember(numbers)) {
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_HAS_DUPLICATE_MEMBER.getMessage());
+        }
+    }
+
+    private boolean checkLottoHasDuplicateMember(List<Integer> numbers) {
+        return numbers.stream()
+                .distinct()
+                .count() != numbers.size();
+    }
+
+    private void validateLottoSize(List<Integer> numbers) {
+        if (checkLottoSize(numbers)) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_SIZE.getMessage());
+        }
+    }
+
+    private static boolean checkLottoSize(List<Integer> numbers) {
+        return numbers.size() != LOTTO_SIZE;
+    }
+}
--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -3,7 +3,9 @@ package lotto.exception;
 public enum ErrorMessage {
     NOT_NUMBER("숫자를 입력해 주세요."),
     INVALID_MONEY("구입 금액은 1,000원으로 나누어 져야합니다."),
-    INVALID_LOTTO_NUMBER("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    INVALID_LOTTO_NUMBER("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    INVALID_LOTTO_SIZE("로또는 6개의 숫자로 이루어 져야합니다."),
+    LOTTO_HAS_DUPLICATE_MEMBER("로또에 중복된 숫자가 포함될 수 없습니다.");
 
     private final String message;
     private final String PREFIX = "[ERROR] ";
